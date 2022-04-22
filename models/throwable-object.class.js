63d6241d107762de35fbc91b896b23e085e9495f
plus <sven.radelof@gmail.com>
2022-04-22 12:27:35 +0000

    update

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -5,7 +5,7 @@ class ThrowableObject extends MovableObject {
 
   constructor(world, x, y) {
     super();
-    this.loadImage("img/7.Marcadores/Icono/Botella.png");
+    this.loadImage('img/7.Marcadores/Icono/Botella.png');
     this.x = x;
     this.y = y;
     this.world = world;
@@ -14,55 +14,63 @@ class ThrowableObject extends MovableObject {
   }
 
   throw() {
+    this.speed = (this.world.character.movingRight || this.world.character.movingLeft) ? 10 : 5;
+    this.move = this.world.character.otherDirection
+      ? this.moveLeft
+      : this.moveRight;
+    this.moveInterval = setInterval(() => {
+      this.move();
+    }, 1000 / 100);
+
     // let pos = this.world.throwableObjects.indexOf(this.world.bottle);
 
-    if (!this.world.character.otherDirection) {
-      if (!this.world.character.movingRight) {
-        for (let i = 0; i < this.world.intervalArray.length; i++) {
-          clearInterval(this.world.intervalArray[i]);
-        }
-        this.world.interval1 = setInterval(() => {
-          this.x += 5;
-          console.log("1");
-        }, 1000 / 100);
-        this.world.intervalArray.push(this.world.interval1);
-      }
-      if (this.world.character.movingRight) {
-        for (let i = 0; i < this.world.intervalArray.length; i++) {
-          clearInterval(this.world.intervalArray[i]);
-        }
-        this.world.interval2 = setInterval(() => {
-          this.x += 10;
-          console.log("2");
-        }, 1000 / 100);
-        this.world.intervalArray.push(this.world.interval2);
-      }
-    }
+    // if (!this.world.character.otherDirection) {
+    //   if (!this.world.character.movingRight) {
+    //     for (let i = 0; i < this.world.intervalArray.length; i++) {
+    //       clearInterval(this.world.intervalArray[i]);
+    //     }
+    //     this.world.interval1 = setInterval(() => {
+    //       this.x += 5;
+    //       console.log("1");
+    //     }, 1000 / 100);
+    //     this.world.intervalArray.push(this.world.interval1);
+    //   }
+    //   if (this.world.character.movingRight) {
+    //     for (let i = 0; i < this.world.intervalArray.length; i++) {
+    //       clearInterval(this.world.intervalArray[i]);
+    //     }
+    //     this.world.interval2 = setInterval(() => {
+    //       this.x += 10;
+    //       console.log("2");
+    //     }, 1000 / 100);
+    //     this.world.intervalArray.push(this.world.interval2);
+    //   }
+    // }
 
-    if (this.world.character.otherDirection) {
-      this.x = this.world.character.x - 30;
-      if (!this.world.character.movingLeft) {
-        for (let i = 0; i < this.world.intervalArray.length; i++) {
-          clearInterval(this.world.intervalArray[i]);
-        }
-        this.world.interval3 = setInterval(() => {
-          this.x -= 5;
-          console.log("3");
-        }, 1000 / 100);
-        this.world.intervalArray.push(this.world.interval3);
-      }
-      if (this.world.character.movingLeft) {
-        for (let i = 0; i < this.world.intervalArray.length; i++) {
-          clearInterval(this.world.intervalArray[i]);
-        }
-        this.world.interval4 = setInterval(() => {
-          this.x -= 10;
-          console.log("4");
-        }, 1000 / 100);
-        this.world.intervalArray.push(this.world.interval4);
-        // console.log(this.world.intervalArray);
-      }
-    }
+    // if (this.world.character.otherDirection) {
+    //   this.x = this.world.character.x - 30;
+    //   if (!this.world.character.movingLeft) {
+    //     for (let i = 0; i < this.world.intervalArray.length; i++) {
+    //       clearInterval(this.world.intervalArray[i]);
+    //     }
+    //     this.world.interval3 = setInterval(() => {
+    //       this.x -= 5;
+    //       console.log("3");
+    //     }, 1000 / 100);
+    //     this.world.intervalArray.push(this.world.interval3);
+    //   }
+    //   if (this.world.character.movingLeft) {
+    //     for (let i = 0; i < this.world.intervalArray.length; i++) {
+    //       clearInterval(this.world.intervalArray[i]);
+    //     }
+    //     this.world.interval4 = setInterval(() => {
+    //       this.x -= 10;
+    //       console.log("4");
+    //     }, 1000 / 100);
+    //     this.world.intervalArray.push(this.world.interval4);
+    //     // console.log(this.world.intervalArray);
+    //   }
+    // }
 
     // setTimeout(() => {
     //   clearInterval(interval1);
