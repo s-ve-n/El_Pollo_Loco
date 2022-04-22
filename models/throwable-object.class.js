24a09ac76ed560f96a7085aa7eb38ac450551bec
plus <sven.radelof@gmail.com>
2022-04-22 21:54:12 +0000

    update

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -8,11 +8,20 @@ class ThrowableObject extends MovableObject {
     'img/6.botella/Rotación/Mesa de trabajo 1 copia 5.png',
     'img/6.botella/Rotación/Mesa de trabajo 1 copia 6.png',
   ];
+  IMAGES_SPLASH = [
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 7.png',
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 8.png',
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 9.png',
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 10.png',
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 11.png',
+    'img/6.botella/Rotación/Splash de salsa/Mesa de trabajo 1 copia 12.png',
+  ];
 
   constructor(x, y) {
     super();
     this.loadImage('img/7.Marcadores/Icono/Botella.png');
     this.loadImages(this.IMAGES_ROTATION);
+    this.loadImages(this.IMAGES_SPLASH);
     this.x = x;
     this.y = y;
     this.throw();
@@ -21,18 +30,27 @@ class ThrowableObject extends MovableObject {
 
   throw() {
     this.speed =
-      world.character.movingRight || world.character.movingLeft
-        ? 10
-        : 5;
-    this.move = world.character.otherDirection
-      ? this.moveLeft
-      : this.moveRight;
-    this.moveInterval = setInterval(() => {
+      world.character.movingRight || world.character.movingLeft ? 10 : 5;
+    this.move = world.character.otherDirection ? this.moveLeft : this.moveRight;
+    setInterval(() => {
       this.move();
     }, 1000 / 100);
-    setInterval(() => {
+    this.rotationInterval = setInterval(() => {
       this.playAnimation(this.IMAGES_ROTATION);
     }, 125);
+      this.splashInterval = setInterval(() => {
+        if (this.y > 300) {
+        this.speed = 0;
+        this.acceleration = 0;
+        this.speedY = 0;
+        clearInterval(this.rotationInterval);
+        this.playAnimation(this.IMAGES_SPLASH);
+        }
+      }, 175);
+      setTimeout(() => {
+        console.log('splash');
+        clearInterval(this.splashInterval);
+      }, 1300);
 
     // let pos = this.world.throwableObjects.indexOf(this.world.bottle);
 
